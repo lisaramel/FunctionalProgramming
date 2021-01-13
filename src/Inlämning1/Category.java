734d69947cf528382107c092cd09697f08411b6c
lisaramel <lisa.ramel@yh.nackademin.se>
2021-01-13 15:55:27 +0100

    first commit

---
+++ src/Inlämning1/Category.java
@@ -0,0 +1,13 @@
+package Inlämning1;
+
+/**
+ * Created by Lisa Ramel
+ * Date: 2021-01-05
+ * Time: 21:08
+ * Project: Inlämningsuppgift1_FP
+ * Copywrite: MIT
+ */
+public enum Category {
+    FOOD, HISTORY, CHEMISTRY
+
+}
---
+++ src/Inlämning1/Question.java
@@ -0,0 +1,41 @@
+package Inlämning1; /**
+ * Created by Lisa Ramel
+ * Date: 2021-01-05
+ * Time: 21:09
+ * Project: Inlämningsuppgift1_FP
+ * Copywrite: MIT
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Question {
+    protected String question;
+    protected Category category;
+    protected List<String> answers = new ArrayList<>();
+    protected int correctAnswer;
+
+    public Question(String q, String a1, String a2, String a3, String a4, Category cat, int correct) {
+        question = q;
+        answers = Arrays.asList(a1, a2, a3, a4);
+        category = cat;
+        correctAnswer = correct;
+    }
+
+    public String getQuestionString(){
+        return question;
+    }
+
+    public Category getCategory(){
+        return category;
+    }
+
+    public int getCorrectAnswer(){
+        return correctAnswer;
+    }
+
+    public List<String> getAllAnswers(){
+        return answers;
+    }
+}
