4059190e2484235763d1964bc562dc902f1334bd
lisaramel <lisa.ramel@yh.nackademin.se>
2021-01-21 15:01:59 +0100

    clean up

--- src/Inlämning2/TheSantaHierarchy.kt
+++ src/Inlämning2/TheSantaHierarchy.kt
@@ -1,7 +1,5 @@
 package Inlämning2
 
-import java.util.Map
-
 /**
  * Created by Lisa Ramel
  * Date: 2021-01-14
@@ -10,26 +8,25 @@ import java.util.Map
  * Copywrite: MIT
  */
 
-class Employee(name: String, isMyBoss: String) {
-    val name = name
-        get() = field
+class Employee(val name: String, var isMyBoss: String)
 
-    var isMyBoss = isMyBoss
-        get() = field
-}
 
-fun findNisse(input: String, list: List<Employee>) : List<String>{
+fun findNisse(input: String, list: List<Employee>): List<String> {
 
     val list2 = mutableListOf<String>()
 
-    fun findHeadOf(names : String, b : Boolean, counter : Int) : List<String>{
+    fun findHeadOf(names: String, b: Boolean, counter: Int): List<String> {
 
-        return if(if(input == "Tomten") list2.add("$names\n" + (list.get(counter).isMyBoss))
-                else if(counter == -1) return list2 else false) list2
-        else findHeadOf(if(list.map { it.name }.contains(input)){ list.get(counter).isMyBoss }
-               else { "" } , list2.add(names), counter - 1).distinct()
+        return if (when {
+            input == "Tomten" -> list2.add("$names\n" + (list.get(counter).isMyBoss))
+            counter == -1 -> return list2
+            else -> false
+        }) list2
+        else findHeadOf(if (list.map { it.name }.contains(input)) {
+            list.get(counter).isMyBoss
+        } else "", list2.add(names), counter - 1).distinct()
     }
-    return findHeadOf("$input" + "s chefer är: ", false, list.map { it.name }.indexOf(input))
+    return findHeadOf(input + "s chefer är: ", false, list.map { it.name }.indexOf(input))
 }
 
 fun main() {
@@ -37,29 +34,29 @@ fun main() {
     val tomten = Employee("Tomten", "Ho-ho, det är jag som är Big Boss här")
     val glader = Employee("Glader", "Tomten")
     val butter = Employee("Butter", "Tomten")
-    val tröger = Employee("Tröger", "Glader")
-    val trötter = Employee("Trötter", "Glader")
+    val troger = Employee("Tröger", "Glader")
+    val trotter = Employee("Trötter", "Glader")
     val blyger = Employee("Blyger", "Glader")
-    val rådjuret = Employee("Rådjuret", "Butter")
+    val radjuret = Employee("Rådjuret", "Butter")
     val nyckelpigan = Employee("Nyckelpigan", "Butter")
     val haren = Employee("Haren", "Butter")
-    val räven = Employee("Räven", "Butter")
+    val raven = Employee("Räven", "Butter")
     val skumtomten = Employee("Skumtomten", "Trötter")
-    val dammråttan = Employee("Dammråttan", "Skumtomten")
-    val gråsuggan = Employee("Gråsuggan", "Räven")
+    val dammrattan = Employee("Dammråttan", "Skumtomten")
+    val grasuggan = Employee("Gråsuggan", "Räven")
     val myran = Employee("Myran", "Räven")
     val bladlusen = Employee("Bladlusen", "Myran")
 
-    var allEmployees = mutableListOf(tomten, glader, butter, tröger, trötter, blyger,
-            rådjuret, nyckelpigan, haren, räven, skumtomten, dammråttan, gråsuggan, myran, bladlusen)
+    val allEmployees = mutableListOf(tomten, glader, butter, troger, trotter, blyger,
+            radjuret, nyckelpigan, haren, raven, skumtomten, dammrattan, grasuggan, myran, bladlusen)
 
-    while(true) {
+    while (true) {
 
         println("Vems chefer vill du se?: ")
-        var input = readLine()!!.toString().toLowerCase().capitalize()
+        val input = readLine()!!.toString().toLowerCase().capitalize()
         println()
 
-        findNisse(input, allEmployees).forEach{ n -> println(n)}
+        findNisse(input, allEmployees).forEach { n -> println(n) }
         println()
 
     }
